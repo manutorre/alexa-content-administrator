6324f5c45794a1bfd71212545f160ba54fd9e3fa
Manuel <manuel.torre@aero.tur.ar>
2018-05-08 01:20:35 -0300

    added category selection final content step

--- src/Components/Sections/Categories.js
+++ src/Components/Sections/Categories.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import {Button} from 'antd'
+import {Button, Select, Option} from 'antd'
 
 export default class Categories extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      categories:[
+        "Deportes",
+        "Política",
+        "Turismo",
+        "Ciencia",
+        "Tecnología"
+      ]
+    }
+  }
+
+
   render(){
+
+    const {Option} = Select
+
     return(
       <div>
         <h1>Categorías</h1>
-        <h3>Ud no tiene categorías disponibles</h3>
+        <Select defaultValue="Deportes" style={{ width: 120 }}>
+          {this.state.categories.map( category => {
+            return(
+              <Option value={category}>{category}</Option>
+            )
+          })}
+        </Select>
         <Button onClick={() => this.props.changeSection("entry")}>Volver</Button>
       </div>
     )
--- src/Components/Sections/Stepper/NewsStepper.js
+++ src/Components/Sections/Stepper/NewsStepper.js
@@ -9,7 +9,8 @@ export default class NewsStepper extends React.Component{
     this.state = {
       showSteps: false,
       currentStep: 1,
-      newsContent: ""
+      confirmedContent: {},
+      selectedCategory: ""
     }
   }
 
@@ -25,17 +26,41 @@ export default class NewsStepper extends React.Component{
     this.setState({currentStep: this.state.currentStep -1})
   }
 
+  selectCategory(categoria){
+    this.setState({
+      selectedCategory: categoria
+    })
+  }
+
+  confirmContent(content){
+    this.setState({
+      confirmedContent:Object.assign(this.state.confirmedContent, content)
+    }, console.log(this.state))
+  }
+
+  showState(){
+    console.log(this.state)
+  }
+
   render(){
     return(
       <div style={{width:"210px",margin:"20px auto"}}>
         <Stepper
           currentStep={this.state.currentStep}
           nextStep={() => this.nextStep()}
-          nextTwoSteps={() => this.nextTwoSteps()} 
-          previousStep={() => this.previousStep()}/>
-        <Button onClick={() => this.props.changeSection("entry")}>
+          nextTwoSteps={() => this.nextTwoSteps()}
+          previousStep={() => this.previousStep()}
+          selectCategory={(value) => this.selectCategory(value)}
+          confirmContent={(content) => this.confirmContent(content)}
+        />
+        <Button style={{display:"inline-block", margin: "5px"}} onClick={() => this.props.changeSection("entry")}>
           Volver
         </Button>
+        {this.state.selectedCategory != "" &&
+          <Button onClick={() => this.showState()}type="danger" style={{display:"inline-block", margin: "5px"}}>
+            Confirmar
+          </Button>
+        }
       </div>
     )
   }
