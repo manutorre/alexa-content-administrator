a6c2bae490892ec30bc885f9d225942514af1309
Manuel <manuel.torre@aero.tur.ar>
2018-05-16 23:48:32 -0300

    añadido filtro por categoría basado en las categorias de los contenidos

--- src/Components/Sections/ContentAdmin/ContentAdmin.js
+++ src/Components/Sections/ContentAdmin/ContentAdmin.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card} from 'antd'
+import {Card, List} from 'antd'
 
 export default class ContentAdmin extends React.Component{
 
@@ -8,6 +8,7 @@ export default class ContentAdmin extends React.Component{
     this.state = {
       confirmedContents: [
         {
+          category:"Deportes",
           title:{
             xpath:"body/div[1]/section[1]/section[1]/article[1]/a[1]/div[1]/h1[1]",
             text:"Benítez: “Lo del ADN, que no se malinterprete, es secundario"
@@ -18,6 +19,7 @@ export default class ContentAdmin extends React.Component{
           }
         },
         {
+          category:"Ciencia",
           title:{
             xpath:"body/div[1]/section[1]/section[1]/article[1]/a[1]/div[1]/h1[1]",
             text:"Benítez: “Lo del ADN, que no se malinterprete, es secundario"
@@ -27,11 +29,19 @@ export default class ContentAdmin extends React.Component{
             text:"nota/79920/benitez_lo_del_adn_que_no_se_malinterprete_es_secundario/"
           }
         }
-      ]
+      ],
+      filteredContents:[]
     }
   }
 
 //mocked data
+
+  filterByCategory(category){
+    let filtered = this.state.confirmedContents.filter( (content) => content.category == category)
+    this.setState({
+      filteredContents: filtered
+    })
+  }
 
   render(){
 
@@ -40,25 +50,41 @@ export default class ContentAdmin extends React.Component{
       display: "flex",
       flexDirection: "column",
       justifyContent: "space-around",
-      width: "70%",
       margin:"20px auto"
     }
+
+    const leftPanelStyle = {
+      border: "1px solid #ebedf0",
+      width:"20%"
+    }
+
+    const allCategories = this.state.confirmedContents.map(content => content.category)
+    const contentsToShow = this.state.filteredContents.length > 0 ? this.state.filteredContents : this.state.confirmedContents
 
     return(
       <div style={globalContainer}>
         <h1 style={{margin: "20px auto"}}>
           Administrador de contenidos
         </h1>
-        <div style={{margin: "20px auto"}}>
-          {this.state.confirmedContents.map( content => {
-            return(
-              <Card
-                style={{ width: 300 }}
-                title={content.title.text}
-                cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-              />
-            )
-          })}
+        <div style={{display:"flex", flexDirection: "row"}}>
+          <div style={leftPanelStyle}>
+            <List
+              dataSource={allCategories}
+              footer={<div onClick={() => this.filterByCategory("")}><a>Mostrar todos</a></div>}
+              renderItem={item => (<List.Item onClick={() => this.filterByCategory(item)}><a>{item}</a></List.Item>)}
+            />
+          </div>
+          <div style={{margin: "20px auto"}}>
+            {contentsToShow.map( content => {
+              return(
+                <Card
+                  style={{ width: 300 }}
+                  title={content.title.text}
+                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                />
+              )
+            })}
+          </div>
         </div>
       </div>
     )
