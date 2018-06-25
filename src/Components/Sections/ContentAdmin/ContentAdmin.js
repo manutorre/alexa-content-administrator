b55a756749f2cbe21bbd11dfa0ede281e44d0e1d
Manuel <manuel.torre@aero.tur.ar>
2018-06-25 00:41:06 -0300

    added react-diagrams

--- src/Components/Sections/ContentAdmin/ContentAdmin.js
+++ src/Components/Sections/ContentAdmin/ContentAdmin.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Card, List, Icon} from 'antd'
+import * as SRD from "storm-react-diagrams"
+require("storm-react-diagrams/dist/style.min.css");
+
 
 export default class ContentAdmin extends React.Component{
 
@@ -112,34 +115,35 @@ export default class ContentAdmin extends React.Component{
     const allCategories = [...new Set(this.state.confirmedContents.map(content => content.category))]
     const contentsToShow = this.state.filteredContents.length > 0 ? this.state.filteredContents : this.state.confirmedContents
 
+    // 1) setup the diagram engine
+    var engine = new SRD.DiagramEngine();
+    engine.installDefaultFactories();
+
+    // 2) setup the diagram model
+    var model = new SRD.DiagramModel();
+
+    // 3) create a default node
+    var node1 = new SRD.DefaultNodeModel("Node 1", "rgb(0,192,255)");
+    let port1 = node1.addOutPort("Out");
+    node1.setPosition(100, 100);
+
+    // 4) create another default node
+    var node2 = new SRD.DefaultNodeModel("Node 2", "rgb(192,255,0)");
+    let port2 = node2.addInPort("In");
+    node2.setPosition(400, 100);
+
+    // 5) link the ports
+    let link1 = port1.link(port2);
+
+    // 6) add the models to the root graph
+    model.addAll(node1, node2, link1);
+
+    // 7) load model into engine
+    engine.setDiagramModel(model);
+
     return(
       <div style={globalContainer}>
-        <h1 style={{margin: "20px auto"}}>
-          Administrador de contenidos
-        </h1>
-        <div style={{display:"flex", flexDirection: "row"}}>
-          <div style={leftPanelStyle}>
-            <List
-              dataSource={allCategories}
-              footer={<div onClick={() => this.filterByCategory("")}><a>Mostrar todos</a></div>}
-              renderItem={item => (<List.Item onClick={() => this.filterByCategory(item)}><a>{item}</a></List.Item>)}
-            />
-          </div>
-          <div style={contentsContainer}>
-            {contentsToShow.map( content => {
-              return(
-                <div>
-                  <Card
-                    style={{ width: 250, margin:"20px" }}
-                    title={content.title.text}
-                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                  />
-                </div>
-              )
-            })}
-          </div>
-        </div>
+        <SRD.DiagramWidget diagramEngine={engine} />;
       </div>
     )
   }
